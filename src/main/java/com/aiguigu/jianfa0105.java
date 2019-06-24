4ec876bbc4babe4933f3e38815161110f5483678
yjmgh <172192326@qq.com>
2019-06-24 21:31:21 +0800

    Initial commit

---
+++ src/main/java/com/aiguigu/jianfa0105.java
@@ -0,0 +1,13 @@
+package com.aiguigu;
+
+
+
+/**
+ * @author Yjm
+ * @create 2019-06-24 21:25
+ */
+public class jianfa0105 {
+    public static void main(String[] args) {
+        System.out.println("hello dugujiujian");
+    }
+}
