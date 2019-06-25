f49dbd87b7f11f5d39a183296c895bbbf0187c81
YuanJiameng0 <1585139115@qq.com>
2019-06-25 09:02:28 +0800

    verson1

---
+++ src/main/java/com/aiguigu/jianfa.java
@@ -0,0 +1,11 @@
+package com.aiguigu;
+
+/**
+ * @author Yjm
+ * @create 2019-06-25 8:56
+ */
+public class jianfa {
+    public static void main(String[] args) {
+        System.out.println("jfirjifjr");
+    }
+}
